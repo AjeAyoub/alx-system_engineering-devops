ba586977407d2c038cacf59c4b9c384715573667
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-11-29 11:24:09 -0800

    RegEx

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
-
-def match_school(arg)
-  regex = /School\z/
-  matches = arg.scan(regex)
-
-  if matches.empty?
-    puts "$"
-  else
-    puts matches.join
-  end
-end
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  match_school(ARGV[0])
-end
+# Reg expression that is matches a given pattern
+puts ARGV[0].scan(/hbt{2,5}n/).join

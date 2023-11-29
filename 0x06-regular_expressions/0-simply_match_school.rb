a191a29f53098829e8109362e756f9addcf8b00d
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-11-29 10:20:10 -0800

    RegEx

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(arg)
-  regex = /School/
-  match = arg.match(regex)
-
-  if match
-    puts match[0] + "$"
-  else
-    puts "$"
-  end
-end
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-else
-  match_school(ARGV[0])
-end
+puts ARGV[0].scan(/School/).join

c9bac741f38c763699ee340555b55ed805ee70aa
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-02-23 14:03:28 -0800

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install puppet-lint
+# 1-install_a_package.pp
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

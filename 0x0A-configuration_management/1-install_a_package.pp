629725c7ae5f3ae26fe45e372d406edb161b7a32
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-02-23 14:19:24 -0800

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup
 # Description: Install Flask package version 2.1.0 using Puppet
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
